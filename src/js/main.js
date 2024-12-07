eb34c57b957a2cc9d7cfaf166956b6d03a1e65bb
Twanivi <aikitashoo@gmail.com>
2024-12-07 20:08:35 +0200

    scss to css

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+const popupIn = document.querySelector('#popup-in');
+const popupUp = document.querySelector('#popup-up');
+
 import { popupLogin } from "./modules/popup-sign-in";
 popupLogin();
 const linkSignUp = document.querySelector('.popup-sign-in__sign-up');
@@ -11,10 +14,12 @@ linkSignUp.addEventListener('click', () => {
 import { popupSignUp } from "./modules/popup-sign-up";
 popupSignUp();
 const linkSignIn = document.querySelector('.popup-sign-in__sign-in');
-linkSignIn.addEventListener('click', () => {
-    popupUp.classList.remove('active');
-    popupIn.classList.add('active');
-})
+if (linkSignUp) {
+    linkSignUp.addEventListener('click', () => {
+        popupIn.classList.remove('active');
+        popupIn.classList.add('active');
+    });
+}
 
 import { changePass } from "./modules/popup-change-pass";
 changePass();
